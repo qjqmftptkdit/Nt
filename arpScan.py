bece35d06a60a5d5a7601bdbb390db8c6e6276d9
Sin, Sungjin <qjqmftptkdit@naver.com>
2019-08-13 00:27:43 +0900

    Arp Scanning 옵션 추가

---
+++ arpScan.py
@@ -0,0 +1,66 @@
+# Arp Scanning을 실행한다.
+def scan(interface, ipRange, option) :
+    g = '\033[92m'
+    e = '\033[0m'
+    v = '\033[95m'
+
+    print(g+"[*] ARP Scanning이 시작됨"+e)
+    from scapy.all import srp,Ether,ARP,conf
+    import random
+    import time
+
+    if option : # Option
+        ipList = getIpList(ipRange)
+    else : # No Option
+        ipList = ipRange
+
+    conf.verb=0
+    ans=[]; unans=[]
+
+    if option=='1' : # Option
+        print(g+"[*] 옵션 : 1~3초간 랜덤으로 간격을 두고 보낸다."+e)
+        for i in range(len(ipList)) :
+            time.sleep(random.randrange(1,4))
+            _ans, _unans = srp(Ether(dst="ff:ff:ff:ff:ff:ff") / ARP(pdst=ipList[i]), iface=interface, timeout=2)
+            if(_ans) : ans.append((_ans[0][0].pdst, _ans[0][1].src))
+            if(_unans) : unans.append(_unans[0].pdst)
+            if(i%10==0) : print(v+"[*] 현재 진행률 : "+str(int(i/len(ipList)*100))+"%"+e)
+    else : # No Option
+        print(g+"[*] 기본 ARP Scanning"+e)
+        ans, unans = srp(Ether(dst="ff:ff:ff:ff:ff:ff") / ARP(pdst=ipList), iface=interface, timeout=2)
+
+    
+    print("총 보낸 주소 갯수 :", len(ans) + len(unans))
+    print("응답된 주소 갯수 :", len(ans))
+    print("응답되지 않은 주소 갯수 :", len(unans))
+    print(g+"[*] 상세정보"+e)
+
+    if option : # Option
+        for pdst, src in ans :
+            print(pdst, " -> ", src)
+    else : # No Option
+        for send, recv in ans :
+            print(send.pdst, " -> ", send.src)
+    
+    print(g+"[*] ARP Scanning이 완료됨"+e)
+
+# ipList를 얻는다.
+def getIpList(ipRange) :
+    ip, ntBits = ipRange.split('/')
+    ipList = []
+
+    if ntBits=="24" :
+        for i in range(256) :
+            ipList.append(".".join(ip.split('.')[:-1]) + "." + str(i))
+    elif ntBits=="16" :
+        for i in range(256) :
+            for j in range(256) :
+                ipList.append(".".join(ip.split('.')[:-2]) + "." + str(i) + "." + str(j))   
+    elif ntBits=="8" :
+        for i in range(256) :
+            for j in range(256) :
+                for z in range(256) :
+                    ipList.append(".".join(ip.split('.')[:-3]) + "." + str(i) + "." + str(j) + "." + str(z))   
+
+    return ipList
+
--- nt.py
+++ nt.py
@@ -1,5 +1,5 @@
 import argparse
-import scan
+import arpScan
 
 def main() :
     parser = argparse.ArgumentParser(description="nt.py -s <ScanNum> [Options]")
@@ -7,13 +7,18 @@ def main() :
     parser.add_argument("-s", "--scantype", type=int, help="스캔타입을 지정한다. 1:Arpscan")
     parser.add_argument("-i", "--interface", help="네트워크 인터페이스를 설정한다.")
     parser.add_argument("-r", "--range", help="네트워크 범위를 지정한다.")
+    parser.add_argument("-o", "--option", help="추가적인 옵션을 지정한다.")
 
     args = parser.parse_args()
     if args.scantype==1 :
         if args.interface and args.range :
-            scan.arpScan(args.interface, args.range)
+            arpScan.scan(args.interface, args.range, args.option)
         else :
-            print("nt.py -s 1 -i <Network InterFace> -r <Ip Range>")
+            print("nt.py -s 1 -i <Network InterFace> -r <Ip Range> [-o option]")
+            print("-o 없음 : 일반 ARP 스캐닝(DEFAULT)")
+            print("-o 1 : 1~3초간 랜덤으로 간격을 두고 보낸다.")
+            print("-o 2 : ip주소를 순서대로 보내지 않고, 무작위로 보낸다.")
+            print("-o 3 : 2,3의 옵션이 결합됨")
             print("ex) nt.py -s 1 -i eth0 -r 192.168.111.1/24")
     else :
         print("nt.py -s <ScanNum> [Options]")
