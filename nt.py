1fe9bc3f3b60d19d2118111ec129b95732db879e
Sin, Sungjin <qjqmftptkdit@naver.com>
2019-08-15 15:09:33 +0900

    타입넘버 출력 옵션 추가

--- nt.py
+++ nt.py
@@ -7,8 +7,8 @@ import icmpScan
 def main() :
     parser = argparse.ArgumentParser(description="nt.py [-s|-a] <TypeNum> [Options]")
 
-    parser.add_argument("-s", "--scantype", type=int, help="스캔타입을 지정한다. 1:Arpscan, 2:IcmpScan")
-    parser.add_argument("-a", "--attacktype", type=int, help="공격타입을 지정한다. 1:ArpSpoofing")
+    parser.add_argument("-s", "--scantype", type=int, help="스캔타입을 지정한다. (0으로 목록확인 가능)")
+    parser.add_argument("-a", "--attacktype", type=int, help="공격타입을 지정한다. (0으로 목록확인 가능)")
     parser.add_argument("-i", "--interface", help="네트워크 인터페이스를 설정한다.")
     parser.add_argument("-r", "--range", help="네트워크 범위를 지정한다.")
     parser.add_argument("-o", "--option", help="추가적인 옵션을 지정한다.")
@@ -16,7 +16,9 @@ def main() :
     parser.add_argument("-g", "--gateway", help="게이트웨이 주소를 지정한다.")
 
     args = parser.parse_args()
-    if args.scantype==1 :
+    if args.scantype==0 :
+        showScanType()
+    elif args.scantype==1 :
         if args.interface and args.range :
             arpScan.scan(args.interface, args.range, args.option)
         else :
@@ -27,6 +29,8 @@ def main() :
         else :
             icmpScan.help()
 
+    elif args.attacktype==0 :
+        showAttackType()
     elif args.attacktype==1 :
         if args.interface and args.target and args.gateway :
             arpSpoofing.ArpSpoofing(args.interface, args.target, args.gateway).startArpSpoofing()
@@ -34,6 +38,15 @@ def main() :
             arpSpoofing.help()
     else :
         print("nt.py [-s|-a] <TypeNum> [Options]")
+
+def showScanType() :
+    print("0 : 스캔타입 목록 출력")
+    print("1 : Arp Scanning [LAN 전용]")
+    print("2 : Icmp Scanning [WAN 전용]")
+
+def showAttackType() :
+    print("0 : 공격타입 목록 출력")
+    print("1 : Arp Spoofing [LAN 전용]")
 
 if __name__ == "__main__" :
     main()
